dd032736036700609eaa8d293ee9067b65fcae7d
Wandera-Martin <martinwandera@gmail.com>
2024-07-06 19:20:54 +0300

    cohere reranking

--- script/envVar.py
+++ script/envVar.py
@@ -1,9 +1,18 @@
-import os
 from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
 load_dotenv()
 
-openai = os.environ('OPENAI_API_KEY')
-langchain = os.environ('LANGCHAIN_API_KEY')
-langchain_tracing = os.environ('LANGCHAIN_TRACING_V2')
+# Access environment variables
+openai_api_key = os.getenv("OPENAI_API_KEY")
+langchain_api_key = os.getenv("LANGCHAIN_API_KEY")
+langchain_tracing_v2 = os.getenv("LANGCHAIN_TRACING_V2")
+cohere_api_key = os.getenv("COHERE_API_KEY")
+tavily_api_key = os.getenv("TAVILY_API_KEY")
 
-print(openai, langchain)
+print(f"OpenAI API Key: {openai_api_key}")
+print(f"LangChain API Key: {langchain_api_key}")
+print(f"LangChain Tracing V2: {langchain_tracing_v2}")
+print(f"Cohere API Key: {cohere_api_key}")
+print(f"Tavily API Key: {tavily_api_key}")
